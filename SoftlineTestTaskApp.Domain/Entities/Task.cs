dec534b04752d1afe09a75d4e573535ee4d273c1
EvgeniyRuban <84354312+EvgeniyRuban@users.noreply.github.com>
2024-02-09 23:44:19 +0600

    Созданы сущности для бд, согласно тз.

---
+++ SoftlineTestTaskApp.Domain/Entities/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SoftlineTestTaskApp.Domain.Entities
+{
+    public sealed class Task : IEntity<Guid>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}
