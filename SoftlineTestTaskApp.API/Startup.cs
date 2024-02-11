e3aadc267e5dc84f73580ac3d9780b093e9df61a
EvgeniyRuban <84354312+EvgeniyRuban@users.noreply.github.com>
2024-02-11 21:44:01 +0600

    Не большие измененения

--- SoftlineTestTaskApp.API/Startup.cs
+++ SoftlineTestTaskApp.API/Startup.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using SoftlineTestTaskApp.DAL;
 using SoftlineTestTaskApp.DAL.Repositories;
 using SoftlineTestTaskApp.Domain.Defenitions;
